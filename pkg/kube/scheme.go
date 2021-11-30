9dcf32dcbcb0281f17ba90e6dd3f21012e93dd98
Nitish Krishna Kaveri <nitish.krishna@getcruise.com>
2021-11-30 10:30:40 -0800

    Add scheme support for cert v1b1

--- pkg/kube/scheme.go
+++ pkg/kube/scheme.go
@@ -30,6 +30,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	certificatesv1 "k8s.io/api/certificates/v1"
+	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
 	coordinationv1beta1 "k8s.io/api/coordination/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	eventsv1beta1 "k8s.io/api/events/v1beta1"
@@ -85,6 +86,7 @@ var (
 		batchv1.AddToScheme,
 		batchv1beta1.AddToScheme,
 		certificatesv1.AddToScheme,
+		certificatesv1beta1.AddToScheme,
 		coordinationv1beta1.AddToScheme,
 		corev1.AddToScheme,
 		eventsv1beta1.AddToScheme,
